56083d45d0b88a8cb343aa8cd0849ca9ce211725
BimsaraG <bimsaragayathraya@gmail.com>
2024-01-19 20:24:50 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string myString = textBox1.Text;
+            string s = myString;
+            var x = myString.Split(' ');
+            listBox1.Items.AddRange(x);
+
+            myString = string.Format("First letter of your text is {0} and the last is {1}", myString[0], myString[myString.Length - 1]);
+
+            for (int i = 0;i <= s.Length;i++)
+            {
+                richTextBox1.Text += s.Substring(0, i) + "\n";
+            }
+            MessageBox.Show(s.Replace("p", "b"));
+            MessageBox.Show(s.PadRight(20, '#'));
+            s = "computer is an electronic information processing machine";
+            MessageBox.Show(s.IndexOf("is").ToString());
+            MessageBox.Show(s.IndexOf("is",15).ToString());
+        }
+    }
+}
